c31d00b3c42dc9a8b66fe3ee342e74b0934d7b20
TheElenek <niemamcinicdopowiedzenia@gmail.com>
2016-11-23 20:28:35 +0100

    Movement + Anim
    Walk left right
    Jump
    Anim doesnt work

---
+++ Assets/scrtipts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movespeed;
+    public float jump;
+    public float duck;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movespeed, 0);
+        }
+         
+        if(Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movespeed, 0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jump);
+            
+        }
+        anim.SetFloat("height", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y));
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            
+        }
+        
+    }
+}
